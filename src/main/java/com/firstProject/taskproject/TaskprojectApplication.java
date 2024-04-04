94cabd8e1ad2f24b3e60dd5e85d3ec0e329f81e8
vismk <vismk@WINDOWS-0I9C3RP.lan>
2024-04-03 19:48:54 -0700

    intial push

---
+++ src/main/java/com/firstProject/taskproject/TaskprojectApplication.java
@@ -0,0 +1,13 @@
+package com.firstProject.taskproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstProject/taskproject/TaskprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstProject.taskproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
